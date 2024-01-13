1edb2154a2ce2f847eb697d05a4b169a03ab81c5
Jan Kadlec <jan.kadlec@agentes-it.com>
2024-01-13 18:50:29 +0100

    Initial commit

---
+++ components/Content.vue
@@ -0,0 +1,229 @@
+<template>
+  <Navbar />
+  <div class="hero min-h-screen" style="background-image: url(/5T0A8211.jpg);">
+    <div class="hero-overlay bg-base-100 bg-opacity-70"></div>
+    <div class="hero-content text-center text-neutral-content flexv h-full">
+
+      <div class="max-w-md flex self-end ">
+        <div class=" flex self-end">
+            <a class="btn btn-primary btn-lg align-bottom mb-10" href="https://share.hsforms.com/11QuDcj41RhaVOkf3Z5Qs-Q5irbi">Become a member</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="contact" class="my-16 flex place-content-center lg:mx-auto mx-5 text-justify">
+    <div class="prose lg:prose-xl text-white prose-headings:text-neutral">
+      <h1 >Contact</h1>
+    </div>
+  </div>
+
+  <div class="my-16 flex place-content-center mx-5 text-justify w-full">
+    <div  class="stats stats-vertical lg:stats-horizontal">
+
+      <div class="stat">
+        <div class="stat-title text-neutral">JCI</div>
+        <div class="stat-value text-neutral">Czech Republic</div>
+        <div class="stat-desc mt-1 text-neutral text-xl">
+          <p class="">Václavské náměstí 795/40</p>
+          <p class=""> Praha 1, 110 00</p>
+          <br />
+          <p cla>IČO: 22885498</p>
+          <p>DIČ: nejsme plátci DPH</p>
+          <br />
+          <p>Email: <a class="underline" href="mailto:info@jcicr.cz">info@jcicr.cz</a></p>
+          <p>Bankovní účet: 2700242309 / 2010</p>
+        </div>
+        <button class="btn btn-square my-4">
+          <a href="https://www.facebook.com/JCICzechRepublic/">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 26 26"
+              style="fill:#EDBE38;">
+              <path
+                d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 20.464844 14.003906 L 18.03125 14.003906 L 18.03125 23.007813 L 13.96875 23.007813 L 13.96875 14.003906 L 12.390625 14.003906 L 12.390625 10.96875 L 13.96875 10.96875 L 13.96875 9.035156 C 13.96875 6.503906 15.019531 5 18.007813 5 L 21.03125 5 L 21.03125 8.023438 L 19.273438 8.023438 C 18.113281 8.023438 18.035156 8.453125 18.035156 9.265625 L 18.03125 10.96875 L 20.796875 10.96875 Z">
+              </path>
+            </svg>
+          </a>
+        </button>
+      </div>
+
+
+      <div  class="stat">
+        <div class="stat-title text-neutral">JCI</div>
+        <div class="stat-value text-neutral">South Bohemia</div>
+        <div class="stat-desc mt-1 text-neutral text-xl">
+          <p class="">Husova tř. 1846/9</p>
+          <p class="">České Budějovice, 371 01</p>
+          <br />
+          <p cla>IČO: 22841601</p>
+          <p>DIČ: nejsme plátci DPH</p>
+          <br />
+          <p>Email: <a class="underline" href="mailto:budejovice@jcicr.cz">budejovice@jcicr.cz</a></p>
+          <p>Bankovní účet: 2300599907 / 2010</p>
+        </div>
+        <button class="btn btn-square my-4">
+          <a href="https://www.facebook.com/jcisb/">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 26 26"
+              style="fill:#EDBE38;">
+              <path
+                d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 20.464844 14.003906 L 18.03125 14.003906 L 18.03125 23.007813 L 13.96875 23.007813 L 13.96875 14.003906 L 12.390625 14.003906 L 12.390625 10.96875 L 13.96875 10.96875 L 13.96875 9.035156 C 13.96875 6.503906 15.019531 5 18.007813 5 L 21.03125 5 L 21.03125 8.023438 L 19.273438 8.023438 C 18.113281 8.023438 18.035156 8.453125 18.035156 9.265625 L 18.03125 10.96875 L 20.796875 10.96875 Z">
+              </path>
+            </svg>
+          </a>
+        </button>
+      </div>
+
+
+      <div class="stat">
+        <div class="stat-title text-neutral">JCI</div>
+        <div class="stat-value text-neutral">Pilsen</div>
+        <div class="stat-desc mt-1 text-neutral text-xl">
+          <p class="">Šafaříkovy sady 2455/5</p>
+          <p class="">Plzeň, 301 00</p>
+          <br />
+          <p cla>IČO: 22710973</p>
+          <p>DIČ: nejsme plátci DPH</p>
+          <br />
+          <p>Email: <a class="underline" href="mailto:info@jcicr.cz">pilsen@jcicr.cz</a></p>
+          <p>Bankovní účet: 2700355952 / 2010</p>
+        </div>
+        <button class="btn btn-square my-4">
+          <a href="https://www.facebook.com/JCIPilsen/">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 26 26"
+              style="fill:#EDBE38;">
+              <path
+                d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 20.464844 14.003906 L 18.03125 14.003906 L 18.03125 23.007813 L 13.96875 23.007813 L 13.96875 14.003906 L 12.390625 14.003906 L 12.390625 10.96875 L 13.96875 10.96875 L 13.96875 9.035156 C 13.96875 6.503906 15.019531 5 18.007813 5 L 21.03125 5 L 21.03125 8.023438 L 19.273438 8.023438 C 18.113281 8.023438 18.035156 8.453125 18.035156 9.265625 L 18.03125 10.96875 L 20.796875 10.96875 Z">
+              </path>
+            </svg>
+          </a>
+        </button>
+      </div>
+
+
+      <div class="stat">
+        <div class="stat-title text-neutral">JCI</div>
+        <div class="stat-value text-neutral">Prague</div>
+        <div class="stat-desc mt-1 text-neutral text-xl">
+          <p class="">Štěpánská 617/26</p>
+          <p class="">Praha 1, 110 00</p>
+          <br />
+          <p cla>IČO: 02506955</p>
+          <p>DIČ: nejsme plátci DPH</p>
+          <br />
+          <p>Email: <a class="underline" href="mailto:prague@jcicr.cz">prague@jcicr.cz</a></p>
+          <p>Bankovní účet: 2100549518 / 2010</p>
+        </div>
+        <button class="btn btn-square my-4">
+          <a href="https://www.facebook.com/JciPrague/">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 26 26"
+              style="fill:#EDBE38;">
+              <path
+                d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 20.464844 14.003906 L 18.03125 14.003906 L 18.03125 23.007813 L 13.96875 23.007813 L 13.96875 14.003906 L 12.390625 14.003906 L 12.390625 10.96875 L 13.96875 10.96875 L 13.96875 9.035156 C 13.96875 6.503906 15.019531 5 18.007813 5 L 21.03125 5 L 21.03125 8.023438 L 19.273438 8.023438 C 18.113281 8.023438 18.035156 8.453125 18.035156 9.265625 L 18.03125 10.96875 L 20.796875 10.96875 Z">
+              </path>
+            </svg>
+          </a>
+        </button>
+      </div>
+
+
+
+
+    </div>
+  </div>
+  
+
+  <!--   <div class=" mb-36 flex place-content-center lg:mx-auto mx-5 text-justify">
+    <div class="prose lg:prose-xl text-info prose-headings:text-primary">
+      <p>Welcome to the official website of JCI EPM 2024 in Prague!</p>
+      <p>JCI (Junior Chamber International) is a worldwide organization bringing together young active individuals
+        who strive to develop their leadership skills and create positive change in their communities. The JCI
+        European Presidents Meeting (EPM) is an annual event that gathers JCI leaders from all over Europe to
+        exchange ideas, share best practices and network.</p>
+      <p>In 2024, JCI EPM will be hosted in the beautiful city of Prague, the capital of the Czech Republic.
+        Prague is known for its stunning architecture, rich history, and vibrant culture. The event will take
+        place from February 8th to February 11th and will feature exciting keynote speakers, interactive
+        workshops, and social events.</p>
+      <p>As a JCI member, attending JCI EPM 2024 in Prague is an opportunity to expand your network, enhance your
+        leadership skills, and experience the beauty of a historic European city. We invite you to join us and
+        be a part of this unforgettable event!</p>
+
+      <h3 class="text-center">Venues</h3>
+      <ul>
+        <li>
+          <h4><a class="text-primary" href="https://www.lobkowicz.cz/en/lobkowicz-palace">Lobkowicz Palace in Prague
+            Castle</a></h4>
+          <p>Jiřská 3, 119 00 Prague 1 <a
+              href="https://www.google.com/maps?q=lobkowicz+palace&safe=active&ssui=on&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiauJqGtYjqAhWoSEEAHUTXD7kQ_AUoAnoECCMQBA">Map</a>
+          </p>
+        </li>
+      </ul>
+      <h3 class="text-center">Accommodation</h3>
+      <ul>
+        <li>
+          <h4><a class="text-primary" href="">OREA Hotel Pyramida Praha</a></h4>
+          <p>Bělohorská 24, 169 01 Praha 6 <a
+              href="https://www.google.com/maps/place/OREA+Hotel+Pyramida+Praha/@50.0842994,14.3789887,17z/data=!4m9!3m8!1s0x470b950e1f5948c1:0x6515a31c9fff100d!5m2!4m1!1i2!8m2!3d50.0854207!4d14.3796371!16s%2Fg%2F1tksm1vw?entry=ttu">Map</a>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div id="tickets">
+    <Tickets/>
+  </div>
+  <div id="tickets_gala">
+    <TicketsGala/>
+  </div>
+
+  <div class="divider"></div>
+  <Organizers/>
+  <div class="divider"></div>
+  <Partners/>
+  <div class="divider"></div>
+  <div class="mt-16 mb-36 flex place-content-center lg:mx-auto mx-5 text-justify">
+    <div  class="mt-10 mb-10 prose lg:prose-xl text-white prose-headings:text-neutral">
+      <h1>Contact</h1>
+      <p class="text-center text-neutral "><a class="link text-neutral" href="mailto:epm24@jcicr.cz ">epm24@jcicr.cz</a>
+      </p>
+      <p class="text-center text-neutral ">
+        <button class="btn btn-square">
+          <a href="https://www.instagram.com/epmprague2024/">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 26 26"
+                 style="fill:#EDBE38;">
+              <path
+                  d="M 7.546875 0 C 3.390625 0 0 3.390625 0 7.546875 L 0 18.453125 C 0 22.609375 3.390625 26 7.546875 26 L 18.453125 26 C 22.609375 26 26 22.609375 26 18.453125 L 26 7.546875 C 26 3.390625 22.609375 0 18.453125 0 Z M 7.546875 2 L 18.453125 2 C 21.527344 2 24 4.46875 24 7.546875 L 24 18.453125 C 24 21.527344 21.53125 24 18.453125 24 L 7.546875 24 C 4.472656 24 2 21.53125 2 18.453125 L 2 7.546875 C 2 4.472656 4.46875 2 7.546875 2 Z M 20.5 4 C 19.671875 4 19 4.671875 19 5.5 C 19 6.328125 19.671875 7 20.5 7 C 21.328125 7 22 6.328125 22 5.5 C 22 4.671875 21.328125 4 20.5 4 Z M 13 6 C 9.144531 6 6 9.144531 6 13 C 6 16.855469 9.144531 20 13 20 C 16.855469 20 20 16.855469 20 13 C 20 9.144531 16.855469 6 13 6 Z M 13 8 C 15.773438 8 18 10.226563 18 13 C 18 15.773438 15.773438 18 13 18 C 10.226563 18 8 15.773438 8 13 C 8 10.226563 10.226563 8 13 8 Z"></path>
+            </svg>
+          </a>
+        </button>
+        <button class="btn btn-square ml-4">
+          <a href="https://www.facebook.com/epm2024/">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="26" height="26" viewBox="0 0 26 26"
+                 style="fill:#EDBE38;">
+              <path
+                  d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 20.464844 14.003906 L 18.03125 14.003906 L 18.03125 23.007813 L 13.96875 23.007813 L 13.96875 14.003906 L 12.390625 14.003906 L 12.390625 10.96875 L 13.96875 10.96875 L 13.96875 9.035156 C 13.96875 6.503906 15.019531 5 18.007813 5 L 21.03125 5 L 21.03125 8.023438 L 19.273438 8.023438 C 18.113281 8.023438 18.035156 8.453125 18.035156 9.265625 L 18.03125 10.96875 L 20.796875 10.96875 Z"></path>
+            </svg>
+          </a>
+        </button>
+      </p>
+    </div>
+  </div>
+ -->
+</template>
+
+<script>
+
+import Tickets from "./Tickets.vue";
+import TicketsGala from "./TicketsGala.vue";
+import Countdown from "./Countdown.vue";
+import Navbar from "./Navbar.vue";
+import Partners from "./Partners.vue";
+import Organizers from "./Organizers.vue";
+
+
+export default {
+  name: "Content",
+  components: { Organizers, Partners, Navbar, TicketsGala, Countdown, Tickets }
+}
+</script>
+
+<style scoped></style>
---
+++ components/Navbar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="z-10 navbar bg-base-100  fixed ">
+    <div class="navbar-start">
+      <details class="dropdown">
+        <summary tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"/>
+          </svg>
+        </summary>
+        <ul class="menu menu-sm dropdown-content mt-3 z-20 p-2 shadow bg-base-100 rounded-box w-52 ">
+  <!--         <li>
+            <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)" to="./#about">About</router-link>
+          </li>
+          <li>
+            <details v-click-outside="subNavbarHide">
+              <summary class="dropdown">Tickets</summary>
+              <ul class="p-2">
+                <li>
+                  <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement)"
+                               to="#tickets">Standard tickets
+                  </router-link>
+                </li>
+                <li>
+                  <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement)"
+                               to="#tickets_gala">Gala tickets
+                  </router-link>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)" to="/#team">Team</router-link>
+          </li><li>
+            <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)" to="/#partners">Partners</router-link>
+          </li>  --><li>
+            <a href="#contact"   @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)" >Contact</a>
+          </li>
+        </ul>
+      </details>
+      <a href="#" class="btn btn-ghost"><img class="h-full px-0" src="/favicon.svg"></a>
+    </div>
+
+    <div class="navbar-center hidden lg:flex ">
+      <ul class="menu menu-horizontal px-1">
+       <!--  <li>
+          <router-link class="text-xl" to="/#about">About</router-link>
+        </li>
+        <li>
+          <details v-click-outside="subNavbarHide">
+            <summary class="text-xl">Tickets</summary>
+            <ul class="p-2">
+              <li>
+                <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)"
+                             class="text-xl" to="#tickets">Standard tickets
+                </router-link>
+              </li>
+              <li>
+                <router-link @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)"
+                             class="text-xl" to="#tickets_gala">Gala tickets
+                </router-link>
+              </li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <router-link class="text-xl" to="/#team">Team</router-link>
+        </li>    <li>
+          <router-link class="text-xl" to="/#partners">Partners</router-link>
+        </li>       -->  <li>
+          <a href="#contact" @click="subNavbarHide($event.target.parentElement.parentElement.parentElement)" >Contact</a>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end ">
+    </div>
+  </div>
+</template>
+<script>
+import Tickets from "./Tickets.vue";
+
+export default {
+  name: "Navbar",
+  components: {Tickets},
+  methods: {
+    subNavbarHide(el) {
+      console.log("subNavbarHide");
+      console.log(el);
+      el.removeAttribute("open");
+    }
+  },
+}
+</script>
+<style scoped>
+
+</style>
