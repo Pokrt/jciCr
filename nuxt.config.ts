bd84931a86ee0452e6fb3e37c0f4de40baa5d070
Jan Kadlec <jan.kadlec@agentes-it.com>
2024-09-11 14:01:52 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+    devtools: {enabled: true},
+    modules: ["@nuxtjs/tailwindcss"],
     css: ['/app.css'],
-  ssr: false,
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'en'
-      }
+    ssr: true,
+    runtimeConfig: {
+        public: {
+            strapiApi: 'http://localhost:1337', // can be overridden by NUXT_PUBLIC_STRAPI_API environment variable
+        }
+    },
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'en'
+            }
+        }
     }
-  }
 })
--- pages/events.vue
+++ pages/events.vue
@@ -16,10 +16,11 @@
     <section class="w-full bg-neutral flex justify-center">
       <div id="events" class="w-full container  px-4 py-12 md:px-6 lg:py-16 bg-neutral ">
         <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3  ">
-          <article v-for="article in articles" class="rounded-lg  border-base-100 border text-base-100">
-            <div v-if="article.image" class="relative h-60 overflow-hidden rounded-t-lg">
+
+          <article v-for="article in articles.data" class="rounded-lg  border-base-100 border text-base-100">
+            <div v-if="article.attributes.Image" class="relative h-60 overflow-hidden rounded-t-lg">
               <img
-                  :src="article.image"
+                  :src="config.public.strapiApi +  article.attributes.Image.data.attributes.formats.medium.url + '?format=webp'"
                   width="340"
                   height="300"
                   class="h-full w-full object-cover object-center"
@@ -27,14 +28,14 @@
               />
             </div>
             <div class="p-4">
-              <h3 class="text-xl font-bold">{{ article.name }}</h3>
+              <h3 class="text-xl font-bold">{{ article.attributes.Name }}</h3>
               <p class="mt-2  ">
-                {{ article.summary }}
+                {{ article.attributes.Summary }}
               </p>
               <NuxtLink
-                  v-if="article.link"
+                  v-if="article.attributes.Link"
                   class="btn text-base-100 bg-white h-10 px-8 hover:btn-neutral mt-4"
-                  :to="article.link"
+                  :to="article.attributes.Link"
               >
                 Více informací
                 <svg
@@ -61,69 +62,76 @@
 
   </main>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      "articles": [
-        {
-          "name": "Leadership meet-ups",
-          "link": null,
-          "summary": "Pravidelné setkání členů JCI v krajských městech. Pro více informací, aktuální termíny a program kontaktujte Lokálního prezidenta.",
-          "image": null
-        },
-        {
-          "name": "Kick off (leden)",
-          "link": null,
-          "summary": "Pravidelný víkendový leadership trénink zaměřený na plánování roku, seberozvoj a týmovou motivaci. Pro členy JCI, zejména pro Lokální a Národní radu.",
-          "image": null
-        },
-        {
-          "name": "Strategická Valná hromada (duben)",
-          "link": null,
-          "summary": "Jednodenní workshop zaměřený na strategii v praxi, měkké dovednosti a leadershipové přístupy. Pro členy JCI na lokální, národní i mezinárodní úrovni.",
-          "image": null
-        },
-        {
-          "name": "Czech academy (květen)",
-          "link": null,
-          "summary": "Tradiční outdoorová leadership akce zaměřená na sebereflexi a osobní růst. Určeno pro členy JCI i zájemce o leadership.",
-          "image": null
-        },
-        {
-          "name": "JCI Regatta - Charitativní závod na plachetnicích",
-          "link": "https://www.jci-regatta.com/",
-          "summary": "Jachtingový závod pro členy, bývalé členy a jejich přátele. Akce je charitativního charakteru.",
-          "image": "https://www.jci-regatta.com/wp-content/uploads/2019/02/IMG_7715.jpg"
-        },
-        {
-          "name": "Evropská akademie JCI ve Švédsku",
-          "link": "https://jciea.jcisweden.se/",
-          "summary": "Exkluzivní leadershipová vzdělávací akce pro mladé leadery ve Švédsku.",
-          "image": null
-        },
-        {
-          "name": "Evropská konference mladých leaderů 2024, Oulu ve Finsku",
-          "link": "https://jciec2024oulu.com/",
-          "summary": "Konference s tématem 'Well-being, technologie, příroda a kultura', kde se setká 2 000 mladých leaderů z celé Evropy.",
-          "image": null
-        },
-        {
-          "name": "Světový kongres mladých leaderů 2024, Tayoun na Taiwanu",
-          "link": "https://www.jciwc24.com/",
-          "summary": "Mladí leadeři z celého světa se setkají na Taiwanu.",
-          "image": null
-        },
-        {
-          "name": "JCI Academy v Japonsku",
-          "link": null,
-          "summary": "Tradiční leadershipová akce s kulturně-diplomatickým přesahem pro národní leadery z více než 120 zemí světa.",
-          "image": null
-        }
-      ]
-    }
+<script setup >
+const config = useRuntimeConfig()
+const { data: articles } = await useFetch(config.public.strapiApi +  '/strapi/events?pagination%5BwithCount%5D=false&populate=%2A')
 
-
-  },
-}
 </script>
+
+
+<!--<script>-->
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      "articles": [-->
+<!--        {-->
+<!--          "name": "Leadership meet-ups",-->
+<!--          "link": null,-->
+<!--          "summary": "Pravidelné setkání členů JCI v krajských městech. Pro více informací, aktuální termíny a program kontaktujte Lokálního prezidenta.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "Kick off (leden)",-->
+<!--          "link": null,-->
+<!--          "summary": "Pravidelný víkendový leadership trénink zaměřený na plánování roku, seberozvoj a týmovou motivaci. Pro členy JCI, zejména pro Lokální a Národní radu.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "Strategická Valná hromada (duben)",-->
+<!--          "link": null,-->
+<!--          "summary": "Jednodenní workshop zaměřený na strategii v praxi, měkké dovednosti a leadershipové přístupy. Pro členy JCI na lokální, národní i mezinárodní úrovni.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "Czech academy (květen)",-->
+<!--          "link": null,-->
+<!--          "summary": "Tradiční outdoorová leadership akce zaměřená na sebereflexi a osobní růst. Určeno pro členy JCI i zájemce o leadership.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "JCI Regatta - Charitativní závod na plachetnicích",-->
+<!--          "link": "https://www.jci-regatta.com/",-->
+<!--          "summary": "Jachtingový závod pro členy, bývalé členy a jejich přátele. Akce je charitativního charakteru.",-->
+<!--          "image": "https://www.jci-regatta.com/wp-content/uploads/2019/02/IMG_7715.jpg"-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "Evropská akademie JCI ve Švédsku",-->
+<!--          "link": "https://jciea.jcisweden.se/",-->
+<!--          "summary": "Exkluzivní leadershipová vzdělávací akce pro mladé leadery ve Švédsku.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "Evropská konference mladých leaderů 2024, Oulu ve Finsku",-->
+<!--          "link": "https://jciec2024oulu.com/",-->
+<!--          "summary": "Konference s tématem 'Well-being, technologie, příroda a kultura', kde se setká 2 000 mladých leaderů z celé Evropy.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "Světový kongres mladých leaderů 2024, Tayoun na Taiwanu",-->
+<!--          "link": "https://www.jciwc24.com/",-->
+<!--          "summary": "Mladí leadeři z celého světa se setkají na Taiwanu.",-->
+<!--          "image": null-->
+<!--        },-->
+<!--        {-->
+<!--          "name": "JCI Academy v Japonsku",-->
+<!--          "link": null,-->
+<!--          "summary": "Tradiční leadershipová akce s kulturně-diplomatickým přesahem pro národní leadery z více než 120 zemí světa.",-->
+<!--          "image": null-->
+<!--        }-->
+<!--      ]-->
+<!--    }-->
+
+
+<!--  },-->
+<!--}-->
+<!--</script>-->
