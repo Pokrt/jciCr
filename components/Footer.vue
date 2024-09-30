e9d1f280ad8f68219ab67495521776b64d953220
Jan Kadlec <jan.kadlec@agentes-it.com>
2024-09-30 17:15:31 +0200

    init

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <footer class="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-    <p class="text-xs text-gray-500 dark:text-gray-400">© 2024 Junior Chamber International.</p>
+    <p class="text-xs text-gray-500 dark:text-gray-400">Developed by <NuxtLink class="link" to="https://www.linkedin.com/in/jan-kadlec/">Jan Kadlec</NuxtLink></p>
     <nav class="sm:ml-auto flex gap-4 sm:gap-6">
-      <a class="text-xs hover:underline underline-offset-4" href="#">
+      <NuxtLink class="text-xs hover:underline underline-offset-4" to="/">
         Domů
-      </a>
-      <a class="text-xs hover:underline underline-offset-4" href="#">
+      </NuxtLink>
+      <NuxtLink class="text-xs hover:underline underline-offset-4" to="/membership">
         Členství
-      </a>
-      <a class="text-xs hover:underline underline-offset-4" href="#">
+      </NuxtLink>
+      <NuxtLink class="text-xs hover:underline underline-offset-4" to="/branches">
         Pobočky & Kontakty
-      </a>
+      </NuxtLink>
     </nav>
   </footer>
 </template>
