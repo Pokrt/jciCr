40a56f90ca45c53b0cd4819d99a90ab0c903711c
Jan Kadlec <jan.kadlec@agentes-it.com>
2024-09-30 19:49:05 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss"],
-    css: ['/app.css'],
-    ssr: true,
-    runtimeConfig: {
-        public: {
-            strapiApi: 'http://localhost:1337', // can be overridden by NUXT_PUBLIC_STRAPI_API environment variable
-        }
-    },
-    app: {
-        head: {
-            htmlAttrs: {
-                lang: 'cs'
-            }
-        }
-    }
+  devtools: {enabled: true},
+  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/sitemap'],
+  css: ['/app.css'],
+  ssr: true,
+
+  runtimeConfig: {
+      public: {
+          strapiApi: 'http://localhost:1337', // can be overridden by NUXT_PUBLIC_STRAPI_API environment variable
+      }
+  },
+
+  app: {
+      head: {
+          htmlAttrs: {
+              lang: 'cs'
+          }
+      }
+  },
+
+  site: {
+      url: 'https://jcicr.cz',
+      name: 'JCI Czech Republic',
+  },
+
+  compatibilityDate: '2024-09-30',
 })
